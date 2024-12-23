693f77606a6686b03e561daf8876bd117bd51492
SrushtiV12 <srushti20pute1@gamil.com>
2024-12-23 21:17:34 +0530

    first commit

--- app/src/main/java/com/theayushyadav11/alat/MainActivity.kt
+++ app/src/main/java/com/theayushyadav11/alat/MainActivity.kt
@@ -1,20 +1,33 @@
 package com.theayushyadav11.alat
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.button.MaterialButton
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
-    }
+
+//        var btn=findViewById<MaterialButton>(R.id.btn_login)
+//        btn.setOnClickListener{
+//            Toast.makeText(this, "Login btn is clicked" , Toast.LENGTH_SHORT).show()
+//
+//            var btn=findViewById<MaterialButton>(R.id.btn_login)
+//            btn.setOnClickListener{
+//                Toast.makeText(this, "Login btn is clicked" , Toast.LENGTH_SHORT).show()
+//            var intent= Intent(this, LoginActivity::class.java)
+//            startActivity(intent)
+//
+//            }
+
+}
 }
---
+++ app/src/main/java/com/theayushyadav11/alat/main.kt
@@ -0,0 +1,12 @@
+package com.theayushyadav11.alat
+
+fun main(){
+    var a="Kotlin"
+//    print (a)
+    sum(2,8)
+}
+
+fun sum(a: Int,b: Int)
+{
+    println(a + b)
+}
